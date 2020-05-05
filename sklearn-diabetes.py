11c7c6c163492469794e527626c65594bcede1c9
Shivlal Sharma <40575397+sshiv5768@users.noreply.github.com>
2020-05-05 22:01:08 +0530

    ML model for diabetes datasets

---
+++ sklearn-diabetes.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error
+
+# ['data', 'target', 'DESCR', 'feature_names', 'data_filename', 'target_filename']
+diabetes = datasets.load_diabetes()
+diabetes_X = diabetes.data
+
+diabetes_X_train = diabetes_X[:-30]
+diabetes_X_test = diabetes_X[-30:]
+
+diabetes_Y_train = diabetes.target[:-30]
+diabetes_Y_test = diabetes.target[-30:]
+
+model = linear_model.LinearRegression()
+model.fit(diabetes_X_train, diabetes_Y_train)
+
+diabetes_Y_predict = model.predict(diabetes_X_test)
+
+print("Mean squared error is: ", mean_squared_error(diabetes_Y_test, diabetes_Y_predict))
+print("Weight: ", model.coef_)
+print("Intercept: ", model.intercept_)
+
+
+# Mean squared error is:  3035.0601152912695
+# Weight:  [941.43097333]
+# Intercept:  153.39713623331698
